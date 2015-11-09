da2b4a014935ab873d4ef752dda856629dcf4ab7
txoof <aaron.ciuffo@gmail.com>
2015-11-09 21:43:46 +0100

    adding experimental files

---
+++ HSV_Ratio_Class_Experiments.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#HSV Sampling of video feed
+
+import cv2
+import numpy as np
+
+#Classes
+class colorHSV:
+    def __init__(self, name, colorRange = 0)
+    # set the default color range
+    self.lower = np.array( [0, 0, 0] )
+    self.upper = np.array( [179, 0, 0])
+    
+    
+    
+
