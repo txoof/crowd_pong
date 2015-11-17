79ab2319066424a31d92fea7de781cd3b89a521c
txoof <aaron.ciuffo@gmail.com>
2015-11-17 10:27:24 +0100

    added score output for testing in twiter-bot

--- write_to_websocket.py
+++ write_to_websocket.py
@@ -8,14 +8,26 @@ import random
 import time
 ws = create_connection("ws://localhost:9000/ws")
 
-def rando():
+def randReal():
     time.sleep(0.4)
     num = (random.random()*2)-1
     return str(num)
 
+def randInt():
+  return str(random.randrange(0, 10))
+
+count = 0 
+
 while True:
-    ws.send(rando())
+    # prevent an overflow
+    if count > 100000:
+      count = 0
+    ws.send(randReal())
+    if count % 20 == 0:
+      for i in range(1):
+        ws.send('SCORE# AI:'+randInt()+'; Human:'+randInt()+'#')
     #result = ws.recv()
+    count += 1
 ws.close
 
 
